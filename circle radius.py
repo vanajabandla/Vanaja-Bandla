ed1d91bfbbb9e1ac45deb89d7554bb1a7eb75583
vanajabandla <78895974+vanajabandla@users.noreply.github.com>
2021-02-11 15:46:07 +0530

    Add files via upload

---
+++ circle radius.py
@@ -0,0 +1,9 @@
+# to find the area of the circle
+from math import pi
+r=float(input("input the radius of the circle:"))
+print ("the area of the circle with radius" + str(r)+"is:"+ str(pi* r**2))
+
+
+
+
+
