70f74b40a9cfddf8c2571e8fb39e8e4e55ed3019
vanajabandla <78895974+vanajabandla@users.noreply.github.com>
2021-02-11 14:53:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-# to find the area of the circle 
-import math
-rad=float(input("enter the radius of the circle:"))
-pi=match.pi
-area=pi*(rad*rad)
-print(f" the area of thr circle with radius (rad) is : (area)")
+   # python program for radius of a circle
+  
+  from math impro pi
+  
+  r=float(input(" enter the radius of circle:"))
+
+  print("area of the circle is: "= str (pi* r * * 2))
+                
+    out put:
+      enter the radius of the circle: 6
+        Area of the circle is : 113.09733552923255
