28648e883455ba7242a261724248fa2a62bc8609
vanajabandla <78895974+vanajabandla@users.noreply.github.com>
2021-02-11 15:34:03 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-   # python program which accepts radius of a circle 
-  
-  from math impro pi
-  
-  r=float(input(" enter the radius of circle:"))
+# print the extension of the file
+file name= input("Input the filename:")
+f_ extns = filename. split(".")
+print("the extension of file is :"repr(f_ extns(-1)))
 
-  print("area of the circle is: "= str (pi* r * * 2))
-                
-    out put:
-      enter the radius of the circle: 6
-        Area of the circle is : 113.09733552923255
+
+
+
