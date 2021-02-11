0a0f5da3b17497ef0cb97e15d9a5d87daf39a5bd
vanajabandla <78895974+vanajabandla@users.noreply.github.com>
2021-02-11 14:40:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,6 @@
-# program to check if a number is prime or not
-number = 407
-# to take input from the user
-# num = int(input(" enter a number: "))
-# prime numbers are greater than 1
-if num > 1
-    # check for factors
-  for i in range (2,num)
-          if (num % i)  == 0
-              print(num," is not a prime number")
-              print(i,'times',num//i,"is",num)
-              break
-          else:
-              print(num,"is a prime number")
-              #if input number is less than
-              #or equal to 1, it is not prime
-          else:
-          print(num,"is not a prime number")
-output 407 is not a prime number
-11 times of 37 is 407
+# to find the area of the circle 
+import math
+rad=float(input("enter the radius of the circle:"))
+pi=match.pi
+area=pi*(rad*rad)
+print(f" the area of thr circle with radius (rad) is : (area)")
